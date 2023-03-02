54dee9eb997903440b16fcb2f632a4a0b4b1cb31
dpinkus87 <dylanpinkus@gmail.com>
2023-03-02 15:34:13 -0600

    trying new things for appending a card to profile...

--- script.js
+++ script.js
@@ -1,31 +1,4 @@
 
-const toyStoryCard = document.getElementById("flip-card").querySelectorAll(".ToyStory");
-console.log(toyStoryCard);
-const elmoCard = document.getElementById("Elmo");
-const natGeoCard = document.getElementById("NatGeo");
-
-// Create ordered list element
-var listEl = document.createElement("ol");
-// Create ordered list items
-var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-var li3 = document.createElement("li");
-
-
-function addTony() {
-    li1.append(toyStoryCard);
-    console.log(li1);
-    // listEl.appendChild(toyStoryCard);
-
-    // listEl.appendChild(elmoCard);
-    // listEl.appendChild(natGeoCard);
-    // window.location.href = "./profile.html";
-
-};
-
-// function goToProfile() {
-//     window.location.href = "./profile.html";
-// }
 
 // SIGNUP & LOGIN FORMS
 function openSignup() {
