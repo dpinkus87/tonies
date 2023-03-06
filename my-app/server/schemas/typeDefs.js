f7a94aca6410278029bdffa0a2b406797749a5aa
dpinkus87 <dylanpinkus@gmail.com>
2023-03-06 10:03:37 -0600

    started typeDefs.js

---
+++ my-app/server/schemas/typeDefs.js
@@ -0,0 +1,28 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Profile {
+        _id: ID
+        name: String
+        email: String
+        password: String
+        tonies: [String]
+    }
+
+    type Auth {
+        token: ID!
+        profile: Profile
+    }
+
+# TODO: Update query
+    type Query {
+    }
+
+type Mutation {
+    addProfile(name: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addTonie(TonieId: ID!): Profile
+}
+`;
+
+module.exports = typeDefs;
