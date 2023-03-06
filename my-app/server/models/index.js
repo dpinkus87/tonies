d6be0eae7927e2c38b5e21414e60254cdb89db4c
dpinkus87 <dylanpinkus@gmail.com>
2023-03-06 15:27:30 -0600

    updated client side - profile, queries, etc.

---
+++ my-app/server/models/index.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const profileSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  tonies: [
+    {
+      type: String,
+      trim: true,
+    },
+  ],
+});
