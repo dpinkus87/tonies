b7870056698b6cff01b9c8c73210fcfbeae51305
dpinkus87 <dylanpinkus@gmail.com>
2023-03-06 09:32:58 -0600

    updated Footer.js component

--- my-app/client/src/components/Footer/index.js
+++ my-app/client/src/components/Footer/index.js
@@ -4,4 +4,21 @@ import { useLocation, useNavigate } from "react-router-dom"
 const Footer = () => {
     const location = useLocation();
     const navigate = useNavigate();
-}
+    return (
+        <footer className=''>
+            <div className=''>
+                {location.pathname !== '/' && (
+                    <button
+                        className='btn btn-dark mb-3'
+                        onClick={() => navigate(-1)}
+                        >
+                            $larr; Go Back
+                        </button>
+                )}
+                <h4>&copy; {new Date().getFullYear()} - RentTonies</h4>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
