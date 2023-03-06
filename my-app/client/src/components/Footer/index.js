9eba0d6bb3ef4f3b296ab713dc7d573d541dbdee
dpinkus87 <dylanpinkus@gmail.com>
2023-03-06 09:15:57 -0600

    updated src index.js

---
+++ my-app/client/src/components/Footer/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { useLocation, useNavigate } from "react-router-dom"
+
+const Footer = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+}
