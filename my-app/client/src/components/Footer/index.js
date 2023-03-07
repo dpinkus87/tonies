6f5ba0e38d6c02a866ec503f896b925d1f4ae9e2
dpinkus87 <dylanpinkus@gmail.com>
2023-03-07 11:57:23 -0600

    added TonieItem component for each Tonie card

--- my-app/client/src/components/Footer/index.js
+++ my-app/client/src/components/Footer/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { useLocation, useNavigate } from "react-router-dom"
 
 const Footer = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
+
     return (
-        <footer className=''>
-            <div className=''>
-                {location.pathname !== '/' && (
-                    <button
-                        className='btn btn-dark mb-3'
-                        onClick={() => navigate(-1)}
-                        >
-                            $larr; Go Back
-                        </button>
-                )}
-                <h4>&copy; {new Date().getFullYear()} - RentTonies</h4>
-            </div>
-        </footer>
+        <Footer
+        className="example"
+        copyrights="&copy 2015 Copyright Text"
+        links={<ul><li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li><li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li><li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li><li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li></ul>}
+        moreLinks={<a className="grey-text text-lighten-4 right" href="#!">More Links</a>}
+      >
+        <h5 className="white-text">
+          Footer Content
+        </h5>
+        <p className="grey-text text-lighten-4">
+          You can use rows and columns here to organize your footer content.
+        </p>
+      </Footer>
     );
 };
 
