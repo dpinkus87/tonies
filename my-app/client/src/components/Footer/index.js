7cba921293e35c3c7ad2411c71353ee330402b34
dpinkus87 <dylanpinkus@gmail.com>
2023-03-07 12:17:04 -0600

    updated footer component

--- my-app/client/src/components/Footer/index.js
+++ my-app/client/src/components/Footer/index.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-
-    return (
-        <Footer
-        className="example"
-        copyrights="&copy 2015 Copyright Text"
-        links={<ul><li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li><li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li><li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li><li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li></ul>}
-        moreLinks={<a className="grey-text text-lighten-4 right" href="#!">More Links</a>}
-      >
-        <h5 className="white-text">
-          Footer Content
-        </h5>
-        <p className="grey-text text-lighten-4">
-          You can use rows and columns here to organize your footer content.
-        </p>
-      </Footer>
-    );
+  return (
+    <Footer
+      className="example"
+      copyrights="&copy 2023 Copyright Text"
+      links={
+        <ul>
+          <li>
+            <a className="grey-text text-lighten-3" href="#!">
+              Link 1
+            </a>
+          </li>
+          <li>
+            <a className="grey-text text-lighten-3" href="#!">
+              Link 2
+            </a>
+          </li>      
+        </ul>
+      }
+      moreLinks={
+        <a className="grey-text text-lighten-4 right" href="#!">
+          More Links
+        </a>
+      }
+    >
+      <h5 className="white-text">Footer Content</h5>
+      <p className="grey-text text-lighten-4">
+        TEST TEST TEST
+      </p>
+    </Footer>
+  );
 };
 
 export default Footer;
