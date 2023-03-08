3e76f870018ebebd61754c37839edb73cae146c4
dpinkus87 <dylanpinkus@gmail.com>
2023-03-08 11:01:47 -0600

    Updated client mutations and queries

--- my-app/client/src/utils/queries.js
+++ my-app/client/src/utils/queries.js
@@ -1,4 +1,58 @@
-// import { gql } from 'apollo/client';
+import { gql } from '@apollo/client';
 
-// export const QUERY_TONIES = gql'
-//     query getTonies()
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
+      _id
+      name
+      description
+      image
+      category {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_ALL_PRODUCTS = gql`
+  {
+    products {
+      _id
+      name
+      description
+      category {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_CATEGORIES = gql`
+  {
+    categories {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_PROFILE = gql`
+  {
+    profile {
+      firstName
+      lastName
+      orders {
+        _id
+        purchaseDate
+        products {
+          _id
+          name
+          description
+          price
+          quantity
+          image
+        }
+      }
+    }
+  }
+`;
