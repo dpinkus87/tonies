136d09d687fcf106aa4ce2aed3db5b3689619d40
dpinkus87 <dylanpinkus@gmail.com>
2023-03-08 08:55:02 -0600

    added Profile model

--- my-app/server/models/Profile.js
+++ my-app/server/models/Profile.js
@@ -2,4 +2,46 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
+const Order = require('./Order');
 
+const profileSchema = new Schema ({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    orders: [Order.schema],
+});
+
+profileSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+  
+  // Compare the incoming password with the hashed password
+  profileSchema.methods.isCorrectPassword = async function (password) {
+    await bcrypt.compare(password, this.password);
+  };
+  
+  const Profile = mongoose.model('Profile', profileSchema);
+  
+  module.exports = Profile;
--- my-app/server/models/index.js
+++ my-app/server/models/index.js
@@ -1,28 +1,6 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const Profile = require('./Profile');
+const Product = require('./Product');
+const Category = require('./Category');
+const Order = require('./Order');
 
-const profileSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, 'Must match an email address!'],
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5,
-  },
-  tonies: [
-    {
-      type: String,
-      trim: true,
-    },
-  ],
-});
+module.exports = { Profile, Product, Category, Order };
