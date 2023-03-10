9b78b57840604ac65ed417f1c6ff9b6a6c11af03
dpinkus87 <dylanpinkus@gmail.com>
2023-03-10 12:39:13 -0600

    updated Footer component

--- my-app/client/src/components/Footer/index.js
+++ my-app/client/src/components/Footer/index.js
@@ -5,29 +5,11 @@ const Footer = () => {
     <Footer
       className="example"
       copyrights="&copy 2023 Copyright Text"
-      links={
-        <ul>
-          <li>
-            <a className="grey-text text-lighten-3" href="#!">
-              Link 1
-            </a>
-          </li>
-          <li>
-            <a className="grey-text text-lighten-3" href="#!">
-              Link 2
-            </a>
-          </li>      
-        </ul>
-      }
-      moreLinks={
-        <a className="grey-text text-lighten-4 right" href="#!">
-          More Links
-        </a>
-      }
+
     >
       <h5 className="white-text">Footer Content</h5>
       <p className="grey-text text-lighten-4">
-        TEST TEST TEST
+        RentTonies.com
       </p>
     </Footer>
   );
